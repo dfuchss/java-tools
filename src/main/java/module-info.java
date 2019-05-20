c468727509ac358ad7214180655d160f37cd0d97
Dominik Fuchß <develop@fuchss.org>
2019-05-20 16:15:00 +0200

    Renamed module

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,5 @@
-module org_fuchss_tools {
-  exports org.fuchss.tools.lambda;
-  exports org.fuchss.tools.misc;
-  exports org.fuchss.tools.tuple;
+module javatools {
+	exports org.fuchss.tools.lambda;
+	exports org.fuchss.tools.misc;
+	exports org.fuchss.tools.tuple;
 }
